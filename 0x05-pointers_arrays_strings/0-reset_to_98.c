a911f5226ffdba6834e835435a15dd228cc45bee
samuelwanjohi <samihpasqal@gmail.com>
2022-09-22 19:09:46 +0300

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * reset_to_98 - sets the value of an int pointer to 98
- * @n: The int pointer to reset
- */
+*reset_to_98 - resets the value of given integer to 98
+*@n: parameter
+*Return : nothing
+*/
+
 void reset_to_98(int *n)
 {
 	*n = 98;
-	b10_pow(10);
 }
